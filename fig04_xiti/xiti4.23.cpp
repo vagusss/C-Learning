3a6c3dd08191be478db271ecb99fe211ce9cdf9d
wang <hnzkwjs@hotmail.com>
2018-01-24 14:31:22 +0800

    习题4.23： else摇摆问题

---
+++ fig04_xiti/xiti4.23.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//part A
+	int x = 5;
+	int y = 8;
+
+	if (y == 8)
+		if (x == 5)
+			cout << "@" << endl;
+		else
+			cout << "#" << endl;
+	cout << "$" << endl;
+	cout << "&" << endl;
+	cout << endl;
+
+	//part B
+	if (y == 8)
+		if (x == 5)
+			cout << "@" << endl;
+		else
+		{
+			cout << "#" << endl;
+			cout << "$" << endl;
+			cout << "&" << endl;
+		}
+	cout << endl;
+
+	//part C
+	if (y == 8)
+		if (x == 5)
+			cout << "@" << endl;
+		else
+		{
+			cout << "#" << endl;
+			cout << "$" << endl;
+		}
+	cout << "&" << endl;
+	cout << endl;
+
+	//part D
+	x = 5;
+	y = 7;
+
+	if (y == 8)
+	{
+		if (x == 5)
+			cout << "@" << endl;
+	}
+	else
+		cout << "#" << endl;
+	cout << "$" << endl;
+	cout << "&" << endl;
+	cout << endl;
+}
